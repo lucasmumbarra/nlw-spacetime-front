6ace5d4ca9ab8be3687bd44018c8599b827e8097
lucasmumbarra <lucassantosm.2014@gmail.com>
2023-08-24 13:36:41 -0300

    Added initial structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="text-2xl font-bold uppercase w-20 h-20 bg-red-500 rounded m-10">
+      oi
+    </div>
+  );
 }
